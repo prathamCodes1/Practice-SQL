998e8798cb42f8b282da233e8f37e52a075c4ca2
Pratham Pancholi <pancholipratham33@gmail.com>
2025-07-09 00:30:35 +0530

    SQL practice

--- 3.sql
+++ 3.sql
@@ -1,3 +1,12 @@
-EXISTS in SQL
+-- EXISTS -> return records if subquery produces one or more records
+-- this returns all those products from the product table if there is entry in the orders table for the product 
+SELECT * from products P WHERE EXISTS (SELECT * FROM orders O where o.product_id = p.id); 
 
-SELECT * from products P where exists (SELECT * from orders O where O.id = P.id); 
+-- ANY -> returns record if the record satisfies the condition with any one of the value returned by subquery 
+-- this returns all the products for which there product_id is greater than any one of the orders
+SELECT * from products P WHERE id > ANY(SELECT id FROM orders); 
+
+
+-- ALL -> returns record if the record satisfies the condition with any one of the value returned by subquery 
+-- this returns all the products for which there product_id is greater than all the orders 
+SELECT * from products P WHERE id > ALL(SELECT id FROM orders);
